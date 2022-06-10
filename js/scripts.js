572f4ea22ebd3790474d942ce8eade22ac63d569
CSW <86068823+noo123m@users.noreply.github.com>
2022-06-10 17:39:32 +0900

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(function(){
+    $(".header_menu>li").mouseenter(function(){
+        $(".subbg").stop().slideDown();
+    });
+    $(".header_menu>li").mouseleave(function(){
+        $(".subbg").stop().slideUp();
+    });
+    
+    $(".header_menu>li").mouseenter(function(){
+        $("#header").css("background","#fff");
+        $("#top").css("background","#fff");
+
+        $(".blacklogo").css("display","block");
+        $(".whitelogo").css("display","none");
+
+        $(".header_menu>li>a").css("color","#232323");
+    });
+
+    $(".header_menu>li").mouseleave(function(){
+        $("#header").css("background","none");
+        $("#top").css("background","none");
+        $(".header_menu>li>a").css("color","#fff");
+        
+        $(".whitelogo").css("display","block");
+        $(".blacklogo").css("display","none");
+
+    });
+
+});
